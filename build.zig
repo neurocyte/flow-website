21abd8f8f12224c1615eb9e81da6ca5306a3433d
plyght <nico.c.jaffer@gmail.com>
2025-02-10 13:13:59 -0500

    Add installer

--- build.zig
+++ build.zig
@@ -2,11 +2,14 @@ const std = @import("std");
 const zine = @import("zine");
 
 pub fn build(b: *std.Build) !void {
-    zine.website(b, .{
-        .title = "Flow Editor",
-        .host_url = "https://flow-editor.github.io",
+    _ = zine.website(b, .{
+        .title = "flow editor",
+        .host_url = "https://flow-editor.dev",
         .content_dir_path = "content",
         .layouts_dir_path = "layouts",
         .assets_dir_path = "assets",
     });
+
+    const install_step = b.addInstallFile(b.path("install"), "install");
+    b.getInstallStep().dependOn(&install_step.step);
 }
